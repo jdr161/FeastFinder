f3c1c565a514a5d9b944c01e2cc892a7988eb086
James Redding <jdr161@case.edu>
2024-03-06 12:37:11 -0500

    formatting changes

--- web-ui/src/routes/homepage/homepage.jsx
+++ web-ui/src/routes/homepage/homepage.jsx
@@ -1,12 +1,12 @@
-import { Container, Typography } from "@mui/material"
+import { Container } from "@mui/material"
 
 function Homepage() {
 
-    return (
-        <Container disableGutters maxWidth="false">
-            <p>
-                hello from the homepage
-            </p>
-        </Container>
-    )
+  return (
+    <Container disableGutters maxWidth="false">
+      <p>
+        hello from the homepage
+      </p>
+    </Container>
+  )
 } export default Homepage
