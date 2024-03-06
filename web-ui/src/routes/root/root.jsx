401eb08820177f2223649e5dee4490cd68653d90
James Redding <jdr161@case.edu>
2024-03-06 12:46:10 -0500

    move authcontext to it's own file

--- web-ui/src/routes/root/root.jsx
+++ web-ui/src/routes/root/root.jsx
@@ -3,7 +3,8 @@ import Footer from '../../components/footer'
 import { Outlet } from 'react-router-dom'
 import { Box, Container, Toolbar } from '@mui/material'
 import { signOut } from 'aws-amplify/auth';
-import { createContext, useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback } from 'react';
+import { AuthContext } from '../../contexts/authcontext';
 
 
 function Root(){
@@ -25,8 +26,6 @@ function Root(){
     setUser,
     logOut,
   }), [user]);
-
-  const AuthContext = createContext(undefined);
 
   return (
     <AuthContext.Provider value={authContextValue}>
